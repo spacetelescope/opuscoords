f10f416e3686d72aa13ce74507656b630fac82df
Joseph Hunkeler <jhunkeler@gmail.com>
2016-03-27 23:19:58 -0400

    Implement RECON

--- setup.py
+++ setup.py
@@ -1,15 +1,45 @@
 #!/usr/bin/env python
+import recon.release
+from glob import glob
+from numpy import get_include as np_include
+from setuptools import setup, find_packages, Extension
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+
+version = recon.release.get_info()
+recon.release.write_template(version, 'lib/opuscoords')
 
 setup(
-    setup_requires=['d2to1>=0.2.5', 'stsci.distutils>=0.3.2'],
-    d2to1=True,
-    use_2to3=False,
-    zip_safe=False
+    name = 'opuscoords',
+    version = version.pep386,
+    author = 'Warren Hack',
+    author_email = 'help@stsci.edu',
+    description = 'Python Tools for OPUS Coordinate Conversions',
+    url = 'https://github.com/spacetelescope/opuscoords',
+    classifiers = [
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires = [
+        'nose',
+        'numpy',
+        'scipy',
+        'sphinx',
+    ],
+
+    package_dir = {
+        '':'lib'
+    },
+    packages = find_packages('lib'),
+    package_data = {
+        '': ['SP_LICENSE']
+    },
+    ext_modules=[
+        Extension('opuscoords.GCcoords',
+            glob('src/*.c'),
+            include_dirs=[np_include()]),
+    ],
 )
